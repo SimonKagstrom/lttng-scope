ab211208a9177473eeb5e74f5feba84261555cef
Simon Kagstrom <simon.kagstrom@gmail.com>
2011-05-16 07:38:18 +0200

    More work on the model

--- src/include/model/model.hh
+++ src/include/model/model.hh
@@ -4,14 +4,10 @@
 #include <wx/dynarray.h>
 #include <wx/hashmap.h>
 
-#include "types.hh"
-
 namespace model
 {
 	class Context;
 	class State;
-
-	class trace::Event;
 
 	WX_DECLARE_OBJARRAY(Context *, ArrayOfContexts);
 	WX_DECLARE_OBJARRAY(State *, ArrayOfStates);
@@ -19,15 +15,27 @@ namespace model
 	class Model
 	{
 	public:
+		class IContextListener
+		{
+		public:
+			virtual void contextSwitch(Context &oldContext, Context &newContext) = 0;
+		};
+
+		class IStateListener
+		{
+		public:
+			virtual void stateSwitch(State &oldState, State &newState) = 0;
+		};
+
 		static Model &getInstance();
 
 		Model();
 
-		void addContext(Context *ctx);
-		void addState(State *state);
+		void addContext(Context &ctx);
+		void addState(State &state);
 
-		Context *contextByEvent(trace::Event *ev);
-		State *stateByEvent(trace::Event *ev);
+		Context *contextByEvent(trace::Event &ev);
+		State *stateByEvent(trace::Event &ev);
 
 	private:
 		ArrayOfContexts m_contexts;
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <trace/event.hh>
 #include <trace/event-type.hh>
 #include <trace/event-data.hh>
+#include <model/model.hh>
 
 int main(int argc, const char *argv[])
 {
